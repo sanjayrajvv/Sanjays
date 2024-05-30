6deed5d315ec34d24c7aee74b487f5377991c7c5
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-05-30 18:50:52 +0530

    Time: 15 ms (74.28%) | Memory: 31.3 MB (33.01%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    int countDinstinct(int i, int j, string s, string t, vector<vector<int>> &dp) {
-        if (j < 0) return 1;
-        if (i < 0) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s[i] == t[j]) {
-            return dp[i][j] = countDinstinct(i - 1, j - 1, s, t, dp) + countDinstinct(i - 1, j, s, t, dp);
-        } else {
-            return dp[i][j] = countDinstinct(i - 1, j, s, t, dp);
-        }
-    }
 public:
     int numDistinct(string s, string t) {
-        int i = s.size(), j = t.size();
+        int n = s.size(), m = t.size();
 
-        vector<vector<int>> dp(i, vector<int>(j, -1));
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
 
-        return countDinstinct(i - 1, j - 1, s, t, dp);
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+ 
+        return (int)dp[n][m];
     }
 };
