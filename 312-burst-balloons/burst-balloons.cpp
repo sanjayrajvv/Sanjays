569db08fbc9423c56ff467a081cde82e2a01d764
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-05 15:41:04 +0530

    Time: 113 ms (67.35%) | Memory: 12 MB (25.42%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -19,8 +19,25 @@ public:
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
 
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
-        return f(1, n, nums, dp);
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j) continue;
+
+                int maxi = INT_MIN;
+                for (int index = i; index <= j; index++) {
+                    int cost = nums[i - 1] * nums[index] * nums[j + 1] 
+                                + dp[i][index - 1]
+                                + dp[index + 1][j];
+
+                    maxi = max(maxi, cost);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
     }
 };
