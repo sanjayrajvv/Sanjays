6c2152e8c3960df7744d2d8263478a48db7d93a7
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-24 12:10:34 +0530

    Time: 4 ms (43.38%) | Memory: 8.8 MB (89.06%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,21 @@
 class Solution {
-    void f(vector<int> &nums, map<int, int> &m, vector<int> &ds, vector<vector<int>> &ans) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (ds.size() == nums.size()) {
-                ans.push_back(ds);
-                return;
-            }
+    void f(int index, vector<int> &nums, vector<vector<int>> &ans) {
+        if (index == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
 
-            if (m[nums[i]] == 0) {
-                ds.push_back(nums[i]);
-                m[nums[i]] = 1;
-                f(nums, m, ds, ans);
-
-                ds.pop_back();
-                m[nums[i]] = 0;
-            }
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[i], nums[index]);
+            f(index + 1, nums, ans);
+            swap(nums[i], nums[index]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        map<int, int> m;
 
-        for (int i = 0; i < nums.size(); i++) m[nums[i]] = 0;
-
-        f(nums, m, ds, ans);
+        f(0, nums, ans);
 
         return ans;
     }
