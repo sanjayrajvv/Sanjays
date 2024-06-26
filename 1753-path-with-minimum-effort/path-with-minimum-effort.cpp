8750e8c9057ff5f571c2eb2b1f609b19c8b51051
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-26 14:43:46 +0530

    Time: 257 ms (9.29%) | Memory: 55.4 MB (9.75%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,45 +1,55 @@
-//Use bfs traverse
-//While traversing carry the min along the way
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
+    bool canReachEnd(vector<vector<int>>& heights, int mid) {
         int rows = heights.size(), columns = heights[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(columns, false));
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        visited[0][0] = true;
 
-        vector<vector<int>> dis(rows, vector<int>(columns, INT_MAX));
-        
-        //{dis,{r,c}};
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
 
-        dis[0][0] = 0;
-        pq.push({0, {0, 0}});
-
-        while (!pq.empty()) {
-            int r = pq.top().second.first;
-            int c = pq.top().second.second;
-            int dist = pq.top().first;
-            pq.pop();
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
 
             if (r == rows - 1 && c == columns - 1) {
-                return dist;
+                return true;
             }
 
-            int delRow[] = {-1, 0, 1, 0};
-            int delCol[] = {0, 1, 0, -1};
-
-            for (int i = 0; i < 4; i ++) {
+            for (int i = 0; i < 4; i++) {
                 int nr = r + delRow[i];
                 int nc = c + delCol[i];
 
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < columns) {
-                    int effort = max(abs(heights[r][c] - heights[nr][nc]), dist);
-                    if (effort < dis[nr][nc]) {
-                        dis[nr][nc] = effort;
-                        pq.push({effort, {nr, nc}});
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < columns && !visited[nr][nc]) {
+                    if (abs(heights[r][c] - heights[nr][nc]) <= mid) {
+                        visited[nr][nc] = true;
+                        q.push({nr, nc});
                     }
                 }
             }
         }
 
-        return -1;
+        return false;
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int left = 0, right = 1e6;
+        int result = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canReachEnd(heights, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
     }
 };
