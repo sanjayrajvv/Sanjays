0b58beac65ff05499284eea80c70c09707788baa
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-27 17:26:49 +0530

    Time: 3 ms (95.00%) | Memory: 12.6 MB (59.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    int f(int r, int c, int m, int n, vector<vector<int>>& grid, vector<vector<int>> &dp) {
-        if (r == m - 1 && c == n - 1) return grid[r][c];
-        if (r >= m || c >= n) return INT_MAX;
-
-        if (dp[r][c] != -1) return dp[r][c];
-
-        int right = f(r, c + 1, m, n, grid, dp);
-        int down = f(r + 1, c, m, n, grid, dp);
-
-        return dp[r][c] = grid[r][c] + min(right, down);
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        return f(0, 0, m, n, grid, dp);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int right = j > 0 ? dp[i][j - 1] : INT_MAX;
+                    int down = i > 0 ? dp[i - 1][j] : INT_MAX;
+
+                    dp[i][j] = grid[i][j] + min(right, down);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
