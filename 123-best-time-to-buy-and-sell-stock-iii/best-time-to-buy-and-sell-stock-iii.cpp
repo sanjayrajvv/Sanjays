4132c14a4868e4b1b09854f71677b60ebeaf95dd
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-28 21:27:14 +0530

    Time: 442 ms (39.24%) | Memory: 222 MB (27.15%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,31 @@
 class Solution {
+    int f(int index, int buy, int count, int n, vector<int>& prices, vector<vector<vector<int>>> &dp) {
+        if (count == 0) return 0;
+        if (index == n) return 0;
+
+        if (dp[index][buy][count] != -1) return dp[index][buy][count];
+
+        int profit = 0;
+        if (buy) {
+            int buyToday = -prices[index] + f(index + 1, !buy, count, n, prices, dp);
+            int notBuy = f(index + 1, buy, count, n, prices, dp);
+            
+            profit = max(buyToday, notBuy);
+        } else {
+            int sellToday = prices[index] + f(index + 1, !buy, count - 1, n, prices, dp);
+            int notSell = f(index + 1, buy, count, n, prices, dp);
+
+            profit = max(sellToday, notSell);
+        }
+
+        return dp[index][buy][count] = profit;
+    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        
-        // Initialize dp array with dimensions [n + 1][4] and fill with 0.
-        vector<int> ahead(5, 0), curr(5, 0);
-        
-        // Iterate over the days from the last to the first
-        for (int index = n - 1; index >= 0; index--) {
-            for (int transaction = 0; transaction < 4; transaction++) {
-                if (transaction % 2 == 0) {
-                    curr[transaction] = max(-prices[index] + ahead[transaction + 1], 
-                                                 ahead[transaction]);
-                } else {
-                    curr[transaction] = max(prices[index] + ahead[transaction + 1], 
-                                                ahead[transaction]);
-                }
-            }
 
-            ahead = curr;
-        }
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
 
-        return ahead[0]; // The maximum profit starting from day 0 with 0 transactions done.
+        return f(0, 1, 2, n, prices, dp);
     }
 };
