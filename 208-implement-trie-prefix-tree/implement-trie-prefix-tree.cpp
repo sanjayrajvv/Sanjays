04a48aeb13ded50a94f458fedd167e3ac57c3941
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-06-30 17:00:17 +0530

    Time: 45 ms (71.27%) | Memory: 48.3 MB (33.91%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -3,7 +3,7 @@ struct Node {
     bool flag = false;
 
     bool containsKey(char ch) {
-        return links[ch - 'a'] != nullptr;
+        return (links[ch - 'a'] != NULL);
     }
 
     void put(char ch, Node *node) {
@@ -30,36 +30,40 @@ public:
         root = new Node();
     }
     
-    void insert(const std::string &word) {
+    void insert(string word) {
         Node *node = root;
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.size(); i++) {
             if (!node->containsKey(word[i])) {
                 node->put(word[i], new Node());
             }
-            node = node->get(word[i]);  // Move to the reference trie
+            node = node->get(word[i]);
         }
-        node->setEnd();  // Set the end of the word
+        node->setEnd();
     }
     
-    bool search(const std::string &word) {
+    bool search(string word) {
         Node *node = root;
         for (int i = 0; i < word.size(); i++) {
             if (!node->containsKey(word[i])) {
                 return false;
             }
+
             node = node->get(word[i]);
         }
+
         return node->isEnd();
     }
     
-    bool startsWith(const std::string &prefix) {
+    bool startsWith(string prefix) {
         Node *node = root;
-        for (int i = 0; i < prefix.length(); i++) {
+        for (int i = 0; i < prefix.size(); i++) {
             if (!node->containsKey(prefix[i])) {
                 return false;
             }
+
             node = node->get(prefix[i]);
         }
+
         return true;
     }
 };
