10b29997936ddc2ab4a3d7a736b5cd050290bcfd
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-04 12:53:45 +0530

    Time: 143 ms (33.45%) | Memory: 73.6 MB (22.12%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -22,8 +22,8 @@ class Solution {
     }
 
     ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode dummy;
-        ListNode* tail = &dummy;
+        ListNode *dummy = new ListNode(1e9);
+        ListNode* tail = dummy;
         
         while (l1 && l2) {
             if (l1->val < l2->val) {
@@ -37,7 +37,7 @@ class Solution {
         }
         
         tail->next = l1 ? l1 : l2;
-        return dummy.next;
+        return dummy->next;
     }
 
     ListNode* mergeSort(ListNode* head) {
