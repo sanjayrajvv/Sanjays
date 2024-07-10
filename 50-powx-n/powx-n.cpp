492423f1e342ddbfadfbe1c486bd025d39a1ac1c
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-10 15:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.00%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <climits>
+
+class Solution {
+    double f(double x, long long n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        
+        double ans = f(x, n / 2);
+
+        if (n % 2 == 1) {
+            return x * ans * ans;
+        } else {
+            return ans * ans;
+        }
+    }
+public:
+    double myPow(double x, int n) {
+        // Handle special case when n is INT_MIN
+        long long N = n;
+        double ans = f(x, abs(N));
+
+        if (n < 0) return 1 / ans;
+        else return ans;
+    }
+};
