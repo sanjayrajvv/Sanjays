5d028d7d7a2aa8e8d0b0a443fe0e4ad11a1cc298
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-15 13:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.76%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -2,19 +2,17 @@ class Solution {
 private:
     int mod = 1e9 + 7;
 private:
-    int f(int n, vector<int> &dp) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        if (n == 3) return 5;
-
-        if (dp[n] != -1) return dp[n];
-
-        return dp[n] = (2 * f(n - 1, dp) % mod + f(n - 3, dp) % mod) % mod;
-    }
 public:
     int numTilings(int n) {
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        if (n > 1) dp[2] = 2; 
+        if (n > 2) dp[3] = 5;
 
-        return f(n, dp);
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] % mod + dp[i - 3] % mod) % mod;
+        }
+
+        return dp[n];
     }
 };
