ee258af04dec78c8cf59d0d1e5668c74720b6191
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-18 16:03:10 +0530

    Time: 26 ms (54.95%) | Memory: 14.3 MB (7.92%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,19 +3,24 @@ public:
     int minimumLength(string s) {
         int i = 0, j = s.size() - 1;
 
+        // Loop until the pointers meet or cross
         while (i < j) {
+            // If the characters at the current pointers are not equal, break the loop
             if (s[i] != s[j]) break;
 
-            while (i + 1 < j && s[i] == s[i + 1]) {
+            // Move inward past all characters equal to s[i] from the left
+            char current_char = s[i];
+            while (i <= j && s[i] == current_char) {
                 i++;
             }
-            i++;
-            while (j - 1 > i && s[j] == s[j - 1]) {
+
+            // Move inward past all characters equal to s[j] from the right
+            while (i <= j && s[j] == current_char) {
                 j--;
             }
-            j--;
         }
 
+        // Return the length of the remaining substring
         return j - i + 1;
     }
 };
