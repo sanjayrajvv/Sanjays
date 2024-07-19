7e1b78a6ad7596ea2a91828804d92d4acc009a91
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-19 15:16:53 +0530

    Time: 71 ms (91.35%) | Memory: 43.9 MB (60.57%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,28 +1,47 @@
 class Solution {
+private:
+    vector<vector<pair<int, int>>> createGraph(int n, vector<vector<int>>& times) {
+        vector<vector<pair<int, int>>> graph(n + 1);
+
+        for (int i = 0; i < times.size(); i++) {
+            int u = times[i][0];
+            int v = times[i][1];
+            int wt = times[i][2];
+
+            graph[u].push_back({v, wt});
+        }
+
+        return graph;
+    }
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> graph = createGraph(n, times);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> dis(n + 1, INT_MAX);
         dis[k] = 0;
 
-        for (int i = 1; i < n; i++) {
-            for (auto edge : times) {
-                int u = edge[0];
-                int v = edge[1];
-                int w = edge[2];
+        pq.push({0, k});
+        while (!pq.empty()) {
+            auto [curDist, node] = pq.top();
+            pq.pop();
 
-                if (dis[u] != INT_MAX && dis[u] + w < dis[v]) {
-                    dis[v] = dis[u] + w;
+            if (curDist > dis[node]) continue;
+
+            for (auto [v, wt] : graph[node]) {
+                if (dis[node] + wt < dis[v]) {
+                    dis[v] = dis[node] + wt;
+                    pq.push({dis[v], v});
                 }
             }
         }
 
-        int maxi = 0;
+        int maxi = INT_MIN;
         for (int i = 1; i <= n; i++) {
             if (dis[i] == INT_MAX) {
                 return -1;
-            } else {
-                maxi = max(maxi, dis[i]);
             }
+
+            maxi = max(maxi, dis[i]);
         }
 
         return maxi;
