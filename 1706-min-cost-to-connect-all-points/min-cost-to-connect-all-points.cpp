a815e2603d652d164196716b0ca0c7bf8ec0c7bb
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-20 16:26:55 +0530

    Time: 479 ms (50.61%) | Memory: 134.5 MB (38.18%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,47 +1,7 @@
-class DisjointSet {
-    vector<int> parent, rank;
-public:
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int findUparent(int node) {
-        if (parent[node] == node) {
-            return node;
-        }
-
-        return parent[node] = findUparent(parent[node]);
-    }
-
-    void Union(int u, int v) {
-        int ulp_u = findUparent(u);
-        int ulp_v = findUparent(v);
-        
-        if (ulp_u == ulp_v) return;
-        else if (rank[ulp_u] > rank[ulp_v]) {
-            parent[ulp_v] = ulp_u;
-        } else if (rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_v]++;
-        }
-    }
-};
-
 class Solution {
-    struct Edges {
-        int weight, from, to;
-    };
-
-    vector<Edges> createEdges(vector<vector<int>>& points) {
-        vector<Edges> edges;
-        int n = points.size();
+private:
+    vector<vector<pair<int, int>>> createGraph(int n, vector<vector<int>>& points) {
+        vector<vector<pair<int, int>>> graph(n);
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -49,42 +9,42 @@ class Solution {
                 int x2 = points[j][0], y2 = points[j][1];
                 int dis = abs(x1 - x2) + abs(y1 - y2);
 
-                edges.push_back({dis, i, j});
+                graph[i].push_back({dis, j});
+                graph[j].push_back({dis, i});
             }
         }
 
-        return edges;
+        return graph;
     }
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        
-        vector<Edges> edges = createEdges(points);
+        vector<vector<pair<int, int>>> graph = createGraph(n, points);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> vis(n, 0);
 
-        //sort(edges.begin(), edges.end());
+        int cost = 0;
+        pq.push({0, 0});
+        while (!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
 
-        sort(edges.begin(), edges.end(), [](const Edges& a, const Edges& b) {
-            return a.weight < b.weight;
-        });
+            if (vis[node]) continue;
 
-        DisjointSet ds(n);
+            vis[node] = 1;
+            cost += dis;
 
-        int mst = 0;
-        for (auto edge : edges) {
-            int w = edge.weight;
-            int u = edge.from;
-            int v = edge.to;
+            for (auto j : graph[node]) {
+                int adj = j.second;
+                int wt = j.first;
 
-            if (ds.findUparent(u) != ds.findUparent(v)) {
-                mst += w;
-
-                ds.Union(u, v);
+                if (!vis[adj]) {
+                    pq.push({wt, adj});
+                }
             }
         }
 
-        return mst;
+        return cost;
     }
 };
-//Create all the edges possible
-//sort them based on thier weight
-//pick each of them and connect without forming a cycle
