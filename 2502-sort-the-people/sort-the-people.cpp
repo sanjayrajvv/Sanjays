bda7080f9b2056fcb1d9823e3f7dc6263fe89602
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-22 13:49:47 +0530

    Time: 23 ms (85.84%) | Memory: 25.5 MB (72.66%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        map<int, string> mp;
-        for (int i = 0; i < heights.size(); i++) {
-            mp[heights[i]] = names[i];
+        int number_of_people = names.size();
+
+        // Create a map to store height-name pairs
+        unordered_map<int, string> height_to_name_map;
+
+        // Populate the map with height as key and name as value
+        for (int i = 0; i < number_of_people; i++) {
+            height_to_name_map[heights[i]] = names[i];
         }
 
-        vector<string> ans;
-        for (auto j : mp) {
-            ans.push_back(j.second);
+        sort(heights.begin(), heights.end());
+
+        vector<string> sorted_names(number_of_people);
+
+        // Populate sorted_names vector in descending order of height
+        for (int i = number_of_people - 1; i >= 0; i--) {
+            sorted_names[number_of_people - i - 1] =
+                height_to_name_map[heights[i]];
         }
 
-        reverse(ans.begin(), ans.end());
-
-        return ans;
+        return sorted_names;
     }
 };
