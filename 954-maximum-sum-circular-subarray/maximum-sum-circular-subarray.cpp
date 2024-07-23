f2cef2f6c2844d9204bfe3414c6fb3c38902a76e
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-23 20:24:00 +0530

    Time: 43 ms (69.22%) | Memory: 42.7 MB (13.95%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        
-        int maxSum = nums[0], currMax = nums[0];
-        int minSum = nums[0], currMin = nums[0];
-        int total = nums[0];
-        
-        for (int i = 1; i < n; ++i) {
-            currMax = max(nums[i], currMax + nums[i]);
-            maxSum = max(maxSum, currMax);
-            
-            currMin = min(nums[i], currMin + nums[i]);
-            minSum = min(minSum, currMin);
-            
-            total += nums[i];
+        int maxSub = INT_MIN, currMaxSum = 0;
+        int minSub = INT_MAX, currMinSum = 0;
+        int total = 0;
+
+        for (int num : nums) {
+            currMaxSum += num;
+            maxSub = max(maxSub, currMaxSum);
+            if (currMaxSum < 0) currMaxSum = 0;
+
+            currMinSum += num;
+            minSub = min(minSub, currMinSum);
+            if (currMinSum > 0) currMinSum = 0;
+
+            total += num;
         }
-        
-        if (maxSum < 0) {
-            return maxSum; // All numbers are negative
-        }
-        
-        return max(maxSum, total - minSum);
+
+        // If all numbers are negative, 
+        //maxSub would be the largest single element.
+        if (maxSub < 0) return maxSub;
+
+        return max(maxSub, total - minSub);
     }
 };
