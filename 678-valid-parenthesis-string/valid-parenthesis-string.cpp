8ad0228c68a57d3e682f181a9e3628ad82a6cf3d
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-23 16:23:15 +0530

    Time: 16 ms (5.42%) | Memory: 29.4 MB (5.02%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    bool f(int index, int count, string s, vector<vector<int>> &dp) {
+        if (count < 0) {
+            return false;
+        }
+
+        if (index == s.size()) {
+            return count == 0;
+        }
+
+        if (dp[index][count] != -1) return dp[index][count];
+
+        bool result = false;
+        if (s[index] == '(') {
+            result =  f(index + 1, count + 1, s, dp);
+        } else if (s[index] == ')') {
+            result =  f(index + 1, count - 1, s, dp);
+        } else {
+            result = f(index + 1, count + 1, s, dp) || 
+            f(index + 1, count - 1, s, dp) || 
+            f(index + 1, count, s, dp); 
+        }
+
+        return dp[index][count] = result;
+    }
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+
+        vector<vector<int>> dp(n, vector<int>(n * 2 + 1, -1));
+
+        return f(0, 0, s, dp);
+    }
+};
