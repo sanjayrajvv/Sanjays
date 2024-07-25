eea0b37ea9e5e63f5e840e6a8f651c92a4d4f841
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-25 18:16:01 +0530

    Time: 62 ms (55.57%) | Memory: 11.5 MB (30.67%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,18 +1,4 @@
 class Solution {
-private:
-    int f(int target, vector<int> &squares, vector<int> &dp) {
-        if (target == 0) return 0;
-        if (dp[target] != -1) return dp[target];
-
-        int mini = INT_MAX;
-        for (int s : squares) {
-            if (target >= s) {
-                mini = min(mini, 1 + f(target - s, squares, dp));
-            }
-        }
-
-        return dp[target] = mini;
-    }
 public:
     int numSquares(int n) {
         if (n == 1) return 1;
@@ -22,8 +8,18 @@ public:
             squares.push_back(i * i);
         }
 
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
+        for (int target = 1; target <= n; target++) {
+            int mini = INT_MAX;
+            for (int s : squares) {
+                if (target >= s) {
+                    mini = min(mini, 1 + dp[target - s]);
+                }
+            }
 
-        return f(n, squares, dp);
+            dp[target] = mini;
+        }
+
+        return dp[n];
     }
 };
