dd2362df46a69c68d4729ebfdd48dd44662684bc
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-25 06:51:46 +0530

    Time: 897 ms (5.00%) | Memory: 275.4 MB (5.17%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,49 +1,28 @@
 class Solution {
-private:
-    vector<vector<pair<int, int>>> createGraph(int n, vector<vector<int>>& times) {
-        vector<vector<pair<int, int>>> graph(n + 1);
-
-        for (int i = 0; i < times.size(); i++) {
-            int u = times[i][0];
-            int v = times[i][1];
-            int wt = times[i][2];
-
-            graph[u].push_back({v, wt});
-        }
-
-        return graph;
-    }
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> graph = createGraph(n, times);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> dis(n + 1, INT_MAX);
         dis[k] = 0;
 
-        pq.push({0, k});
-        while (!pq.empty()) {
-            auto [curDist, node] = pq.top();
-            pq.pop();
+        for (int i = 1; i < n; i++) {
+            for (auto edge : times) {
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
 
-            if (curDist > dis[node]) continue;
-
-            for (auto [v, wt] : graph[node]) {
-                if (dis[node] + wt < dis[v]) {
-                    dis[v] = dis[node] + wt;
-                    pq.push({dis[v], v});
+                if (dis[u] != INT_MAX && dis[u] + wt < dis[v]) {
+                    dis[v] = dis[u] + wt;
                 }
             }
         }
 
-        int maxi = INT_MIN;
+        int maxTime = 0;
         for (int i = 1; i <= n; i++) {
-            if (dis[i] == INT_MAX) {
-                return -1;
-            }
+            if (dis[i] == INT_MAX) return -1;
 
-            maxi = max(maxi, dis[i]);
+            maxTime = max(maxTime, dis[i]);
         }
 
-        return maxi;
+        return maxTime;
     }
 };
