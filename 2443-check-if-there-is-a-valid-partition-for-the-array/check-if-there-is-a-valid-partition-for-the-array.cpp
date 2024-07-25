bfda38cae4dcb6dd0762d9e30159c71573516164
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-25 19:52:31 +0530

    Time: 128 ms (30.56%) | Memory: 86.8 MB (76.04%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,31 +1,27 @@
 class Solution {
 private:
     bool isGood(vector<int> &nums, int l, int r) {
-        if (r - l + 1 == 2) {
-            if (nums[l] == nums[r]) {
-                return true;
-            }
-        } else if (r - l + 1 == 3) {
-            if (nums[l] == nums[l + 1] && nums[l + 1] == nums[l + 2]) {
-                return true;
-            } else if (nums[l] + 1 == nums[l + 1] && nums[l + 1] + 1 == nums[l + 2]) {
-                return true;
-            }
+        int len = r - l + 1;
+        if (len == 2) {
+            return nums[l] == nums[r];
+        } else if (len == 3) {
+            return (nums[l] == nums[l + 1] && nums[l + 1] == nums[l + 2]) ||
+                   (nums[l] + 1 == nums[l + 1] && nums[l + 1] + 1 == nums[l + 2]);
         }
         return false;
     }
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
 
-        vector<bool> dp(n + 1, 0);  // Change size to n+1
-        dp[n] = true;  // Initialize the end state as true
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int i = index; i < min(index + 3, (int)nums.size()); i++) {
-                if (isGood(nums, index, i)) {
-                    if (dp[i + 1]) {
+        for (int index = n - 1; index >= 0; --index) {
+            for (int len = 2; len <= 3; ++len) {
+                if (index + len <= n && isGood(nums, index, index + len - 1)) {
+                    if (dp[index + len]) {
                         dp[index] = true;
+                        break; // Exit early since we found a valid partition
                     }
                 }
             }
