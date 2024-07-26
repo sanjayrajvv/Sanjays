74a6cb7692f8e6ad0e22dfc5da956b64351bd70a
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 22:02:10 +0530

    Time: 55 ms (19.33%) | Memory: 47 MB (35.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,26 @@
 class Solution {
-private:
-    int helper(int index, int amount, vector<int>& coins, vector<vector<int>> &dp) {
-        if (index == 0) {
-            if (amount % coins[index] == 0) return amount / coins[index];
-            else return 1e9;
-        }
-
-        if (dp[index][amount] != -1) return dp[index][amount];
-
-        int take = 1e9;
-        if (amount >= coins[index]) {
-            take = 1 + helper(index, amount - coins[index], coins, dp);
-        }
-        int notTake = helper(index - 1, amount, coins, dp);
-
-        return dp[index][amount] = min(take, notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
+        for (int a = 0; a <= amount; a++) {
+            if (a % coins[0] == 0) dp[0][a] = a / coins[0];
+        }
 
-        int count = helper(n - 1, amount, coins, dp);
-        if (count == 1e9) return -1;
-        else return count;
+        for (int index = 1; index < n; index++) {
+            for (int a = 0; a <= amount; a++) {
+                int take = 1e9;
+                if (a >= coins[index]) {
+                    take = 1 + dp[index][a - coins[index]];
+                }
+                int notTake = dp[index - 1][a];
+
+                dp[index][a] = min(take, notTake);
+            }
+        }
+
+        int count = dp[n - 1][amount];
+        return (count == 1e9) ? -1 : count;
     }
 };
