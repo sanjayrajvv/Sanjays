dddf539e33571d60d1bbfb49f20e701f92d01c1c
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 10:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (16.30%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,23 @@
 class Solution {
-    int f(int i, int j, int m, int n, vector<vector<int>> &dp) {
-        if (i < 0 || i >= m || j < 0 || j >= n) return 0;
-        if (i == 0 && j == 0) return 1;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int count = 0;
-        count += f(i - 1, j, m, n, dp); //up
-        count += f(i, j - 1, m, n, dp); //left
-
-        return dp[i][j] = count;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        return f(m - 1, n - 1, m, n, dp);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int count = 0;
+                    if (i - 1 >= 0) count += dp[i - 1][j]; //up
+                    if (j - 1 >= 0) count += dp[i][j - 1]; //left
+
+                    dp[i][j] = count;
+                }
+
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
