5b22d6b08a23babebb1796793c4a6f4e375f129f
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 23:32:54 +0530

    Time: 3 ms (67.98%) | Memory: 8.7 MB (25.60%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        return XORSum(nums, 0, 0); 
-    }
-
-private:
-    int XORSum(vector<int>& nums, int index, int currentXOR) {
-        // Return currentXOR when all elements in nums are already considered
-        if (index == nums.size()) return currentXOR;
-
-        // Calculate sum of subset xor with current element
-        int withElement = XORSum(nums, index + 1, currentXOR ^ nums[index]);
-
-        // Calculate sum of subset xor without current element
-        int withoutElement = XORSum(nums, index + 1, currentXOR);
-
-        // Return sum of xor totals
-        return withElement + withoutElement;
+        int result = 0;
+        // Capture each bit that is set in any of the elements
+        for (int num : nums) {
+            result |= num;
+        }
+        // Multiply by the number of subset XOR totals that will have each bit set
+        return result << (nums.size() - 1);
     }
 };
