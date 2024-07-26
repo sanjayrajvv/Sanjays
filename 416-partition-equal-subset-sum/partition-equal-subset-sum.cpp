ab1020f0d45c2d8055604b200db84f1fd0b3ae1f
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 19:13:25 +0530

    Time: 124 ms (73.68%) | Memory: 99 MB (30.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,39 +1,28 @@
 class Solution {
-    bool f(int index, int target, vector<int> &nums, vector<vector<int>> &dp) {
+private:
+    int helper(int index, int target, vector<int> &nums, vector<vector<int>> &dp) {
         if (target == 0) return true;
-        if (index == 0) return (nums[0] == target);
+        if (index == 0) {
+            return nums[0] == target;
+        }
 
         if (dp[index][target] != -1) return dp[index][target];
 
-        int notTake = f(index - 1, target, nums, dp);
-        int take = false;
-        if (nums[index] <= target) take = f(index - 1, target - nums[index], nums, dp);
+        bool notTake = helper(index - 1, target, nums, dp);
+        bool take = false; 
+        if (target >= nums[index]) take = helper(index - 1, target - nums[index], nums, dp);
 
-        return dp[index][target] = take | notTake;
+        return dp[index][target] = notTake || take;
     }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-
-        if (sum % 2 == 1) return false;
-
+        if (sum % 2) return false;
         int target = sum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
 
-        for (int i = 0; i < n; i++) dp[i][0] = true;
-
-        for (int index = 1; index < n; index++) {
-            for (int t = 0; t <= target; t++) {
-                int notTake = dp[index - 1][t];
-                int take = false;
-                if (nums[index] <= t) take = dp[index - 1][t - nums[index]];
-
-                dp[index][t] = take | notTake;
-            }
-        }
-
-        return dp[n - 1][target];
+        return helper(n - 1, target, nums, dp);
     }
 };
