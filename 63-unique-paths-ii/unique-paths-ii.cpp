952376a16fac4822f171413bf636e965abb58218
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 14:36:57 +0530

    Time: 3 ms (53.95%) | Memory: 10.5 MB (17.70%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,36 +1,30 @@
 class Solution {
-    int paths(int r, int c, int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp) {
-        if (r == m - 1 && c == n - 1) return 1;
-        if (r >= m || c >= n ) return 0;
-
-        if (dp[r][c] != -1) return dp[r][c];
-
-        int right = 0, down = 0;
-        if (r + 1 < m && obstacleGrid[r + 1][c] == 0) right = paths(r + 1, c, m, n, obstacleGrid, dp);
-        if (c + 1 < n && obstacleGrid[r][c + 1] == 0) down = paths(r, c + 1, m, n, obstacleGrid, dp);
-
-        return dp[r][c] = right + down;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-
-        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
 
         vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        dp[0][0] = 1;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (obstacleGrid[r][c] == 1) {
-                    dp[r][c] = 0; 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
                 } else {
-                    if (r > 0) dp[r][c] += dp[r-1][c];
-                    if (c > 0) dp[r][c] += dp[r][c-1]; 
+                    int count = 0;
+                    if (i - 1 >= 0) {
+                        count += dp[i - 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        count += dp[i][j - 1];
+                    }
+                    dp[i][j] = count;
                 }
             }
         }
 
         return dp[m - 1][n - 1];
-    }
+     }
 };
