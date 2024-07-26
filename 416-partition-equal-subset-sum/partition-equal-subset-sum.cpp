462d56b0a0df4ff02996fe715c2c65a06516348c
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 19:26:49 +0530

    Time: 267 ms (32.54%) | Memory: 12.9 MB (89.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -6,22 +6,24 @@ public:
         if (sum % 2) return false;
         int target = sum / 2;
 
-        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
-        for (int i = 0; i < n; i++) dp[i][0] = true;
-        if (nums[0] <= target) dp[0][nums[0]] = true;
+        vector<bool> prev(target + 1, false);
+        vector<bool> curr(target + 1, false);
+        prev[0] = curr[0] = true;
+        if (nums[0] <= target) prev[nums[0]] = true;
 
         for (int index = 1; index < n; index++) {
             for (int currTarget = 0; currTarget <= target; currTarget++) {
-                bool notTake = dp[index - 1][currTarget];
+                bool notTake = prev[currTarget];
                 bool take = false; 
                 if (currTarget >= nums[index]) {
-                    take = dp[index - 1][currTarget - nums[index]];
+                    take = prev[currTarget - nums[index]];
                 }
 
-                dp[index][currTarget] = notTake || take;
+                curr[currTarget] = notTake || take;
             }
+            prev = curr;
         }
 
-        return dp[n - 1][target];
+        return prev[target];
     }
 };
