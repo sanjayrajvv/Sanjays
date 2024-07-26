294d14963d36c095b284de12a564f13a05193ae4
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-26 15:38:50 +0530

    Time: 4 ms (73.38%) | Memory: 11.8 MB (11.91%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,16 +1,12 @@
 class Solution {
+private:
     int f(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>> &dp) {
-        if (i == n - 1) {
-            return triangle[i][j];
-        }
-
-        if (i == n) return 0;
+        if (i == n - 1) return triangle[i][j];
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        int down = INT_MAX, diag = INT_MAX;
-        if (i + 1 < n) down = f(i + 1, j, n, triangle, dp);
-        if (i + 1 < n && j + 1 <= i + 1) diag = f(i + 1, j + 1, n, triangle, dp);
+        int down = f(i + 1, j, n, triangle, dp);
+        int diag = f(i+ 1, j + 1, n, triangle, dp);
 
         return dp[i][j] = triangle[i][j] + min(down, diag);
     }
