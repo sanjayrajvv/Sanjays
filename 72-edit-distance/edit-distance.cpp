7d5f393bf34a5c647cf55ee496aa112d0b73949d
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-27 11:46:16 +0530

    Time: 56 ms (11.09%) | Memory: 83.1 MB (5.01%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,38 +1,26 @@
 class Solution {
-    int countDist(int i, int j, string word1, string word2, vector<vector<int>> &dp) {
-        if (i < 0) return j + 1;
-        if (j < 0) return i + 1;
+private:
+    int helper(int i, int j, string word1, string word2, vector<vector<int>> &dp) {
+        if (i == 0) return j;
+        if (j == 0) return i;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (word1[i] == word2[j]) return dp[i][j] = countDist(i - 1, j - 1, word1, word2, dp);
-        else {
-            return dp[i][j] = min({(1 + countDist(i, j - 1, word1, word2, dp)), 
-            (1 + countDist(i - 1, j, word1, word2, dp)), 
-            (1 + countDist(i - 1, j - 1, word1, word2, dp))});
+        int minOp;
+        if (word1[i - 1] == word2[j - 1]) {
+            minOp =  helper(i - 1, j - 1, word1, word2, dp);
+        } else {
+            minOp = 1 + min({helper(i - 1, j, word1, word2, dp), 
+            helper(i, j - 1, word1, word2, dp), helper(i - 1, j - 1, word1, word2, dp)});
         }
+        return dp[i][j] = minOp;
     }
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
+        int n1 = word1.size(), n2 = word2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1 , -1));
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
 
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int j = 0; j <= m; j++) dp[0][j] = j;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1[i - 1] == word2[j - 1]) 
-                    dp[i][j] = dp[i - 1][j - 1]; 
-                else {
-                    dp[i][j] = min({(1 + dp[i][j - 1]), 
-                    (1 + dp[i - 1][j]),
-                    (1 + dp[i - 1][j - 1])});
-                }
-            }
-        }
-
-        return dp[n][m];
+        return helper(n1, n2, word1, word2, dp);
     }
 };
