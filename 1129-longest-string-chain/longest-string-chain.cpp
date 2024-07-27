bc1366dc430176f511b62597bbbbc1a8c02038e1
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-27 22:23:26 +0530

    Time: 54 ms (76.63%) | Memory: 17.2 MB (63.97%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,38 +1,39 @@
 class Solution {
-    bool checkPossible(string word1, string word2) {
-        if (word1.size() != word2.size() + 1) return false;
+private:
+    bool isPredecessor(string &a, string &b) {
+        if (a.size() + 1 != b.size()) return false;
 
-        int first = 0;
-        int second = 0;
-        while (first < word1.size()) {
-            if (second < word2.size() && word1[first] == word2[second]) {
-                first++;
-                second++;
+        int n1 = a.size(), n2 = b.size();
+        int i = 0, j = 0;
+        while (j < n2) {
+            if (i < n1 && a[i] == b[j]) {
+                i++;
+                j++;
             } else {
-                first++;
+                j++;
             }
         }
 
-        if (first == word1.size() && second == word2.size()) return true;
+        if (i == n1 && j == n2) return true;
         else return false;
     }
 public:
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        vector<int> dp(n, 1);
 
-        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+        sort(words.begin(), words.end(), [](const string &a, const string &b) {
             return a.size() < b.size();
         });
 
-        int len = INT_MIN;
-        for (int index = 0; index < n; index++) {
-            for (int prev_index = 0; prev_index < index; prev_index++) {
-                if (checkPossible(words[index], words[prev_index]) && 1 + dp[prev_index] > dp[index]) {
-                    dp[index] = 1 + dp[prev_index];
+        int len = 1;
+        vector<int> dp(n, 1);
+        for (int index = 1; index < n; index++) {
+            for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+                if (isPredecessor(words[prevIndex], words[index]) && 
+                (1 + dp[prevIndex] > dp[index])) {
+                    dp[index] = 1 + dp[prevIndex];
                 }
             }
-
             len = max(len, dp[index]);
         }
 
