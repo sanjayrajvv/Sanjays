c2c105fc504baab59f272835b7deb708c28993f2
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-27 17:10:53 +0530

    Time: 454 ms (36.62%) | Memory: 210.1 MB (38.45%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,31 @@
 class Solution {
-private:
-    int f(int index, bool buy, int count, vector<int> &prices, 
-    vector<vector<vector<int>>> &dp) {
-        if (index == prices.size() || count == 0) return 0;
-
-        if (dp[index][buy][count] != -1) return dp[index][buy][count];
-
-        int profit = 0;
-        if (buy) {
-            int buyToday = -prices[index] + f(index + 1, !buy, count, prices, dp);
-            int notBuyToday = f(index + 1, buy, count, prices, dp);
-
-            profit = max(buyToday, notBuyToday);
-        } else {
-            int sellToday = prices[index] + f(index + 1, !buy, count - 1, prices, dp);
-            int notSellToday = f(index + 1, buy, count, prices, dp);
-
-            profit = max(sellToday, notSellToday);
-        }
-
-        return dp[index][buy][count] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
 
-        return f(0, 1, 2, prices, dp);
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int count = 1; count <= 2; count++) {
+                    int profit = 0;
+                    if (buy) {
+                        int buyToday = -prices[index] + dp[index + 1][!buy][count];
+                        int notBuyToday = dp[index + 1][buy][count];
+
+                        profit = max(buyToday, notBuyToday);
+                    } else {
+                        int sellToday = prices[index] + dp[index + 1][!buy][count - 1];
+                        int notSellToday = dp[index + 1][buy][count];
+
+                        profit = max(sellToday, notSellToday);
+                    }
+
+                    dp[index][buy][count] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
