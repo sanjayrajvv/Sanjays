bb80c85fc388448962c996fda49090b898f4bc51
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-30 16:39:28 +0530

    Time: 6 ms (65.71%) | Memory: 10.7 MB (72.05%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,24 +1,28 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 private:
     const int MOD = 1000000007;
-
-    int f(int i, int low, int high, int zero, int one, vector<int>& memo) {
-        if (i > high) return 0;
-
-        if (memo[i] != -1) return memo[i];
-
-        int count = 0;
-        if (i >= low && i <= high) count = 1;
-
-        count = (count + f(i + zero, low, high, zero, one, memo) % MOD) % MOD;
-        count = (count + f(i + one, low, high, zero, one, memo) % MOD) % MOD;
-
-        memo[i] = count;
-        return memo[i];
-    }
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> memo(high + 1, -1);
-        return f(0, low, high, zero, one, memo);
+        vector<int> memo(high + 1, 0);
+        memo[0] = 1;  // There's one way to construct a string of length 0, the empty string
+
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero) {
+                memo[i] = (memo[i] + memo[i - zero]) % MOD;
+            }
+            if (i >= one) {
+                memo[i] = (memo[i] + memo[i - one]) % MOD;
+            }
+        }
+
+        int result = 0;
+        for (int i = low; i <= high; i++) {
+            result = (result + memo[i]) % MOD;
+        }
+
+        return result;
     }
 };
