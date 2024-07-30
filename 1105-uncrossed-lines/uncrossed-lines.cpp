1726b3f57a377ddc49edbb091463298d456843b7
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-30 15:15:10 +0530

    Time: 59 ms (5.01%) | Memory: 15.5 MB (27.86%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,28 +1,26 @@
 class Solution {
-private:
-    int f(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp) {
-        if (i >= nums1.size() || j >= nums2.size()) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int notMatch = f(i + 1, j, nums1, nums2, dp);
-        int match = 0;
-        for (int index = j; index < nums2.size(); index++) {
-            if (nums1[i] == nums2[index]) {
-                match = 1 + f(i + 1, index + 1, nums1, nums2, dp);
-                break;
-            }
-        }
-
-        return dp[i][j] = max(notMatch, match);
-    }
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
 
-        return f(0, 0, nums1, nums2, dp);
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int notMatch = dp[i + 1][j];
+                int match = 0;
+                for (int index = j; index < nums2.size(); index++) {
+                    if (nums1[i] == nums2[index]) {
+                        match = 1 + dp[i + 1][index + 1];
+                        break;
+                    }
+                }
+
+                dp[i][j] = max(notMatch, match);
+            }
+        }
+
+        return dp[0][0];
     }
 };
