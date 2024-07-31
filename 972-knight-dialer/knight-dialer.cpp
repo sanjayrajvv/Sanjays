46c238dfeec43d1784f439b1828f5c3070059b2d
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-31 07:06:48 +0530

    Time: 81 ms (61.97%) | Memory: 39.1 MB (37.66%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -14,30 +14,26 @@ private:
         {1, 3},
         {2, 4}
     };
-
-    int f(int rem, int square) {
-        if (rem == 0) {
-            return 1;
-        }
-
-        if (dp[rem][square] != -1) {
-            return dp[rem][square];
-        }
-
-        int ans = 0;
-        for (int nextSquare : jumps[square]) {
-            ans = (ans + f(rem - 1, nextSquare)) % MOD;
-        }
-
-        dp[rem][square] = ans;
-        return ans;
-    }
+    
 public:
     int knightDialer(int n) {
-        dp = vector(n + 1, vector<int>(10, -1));
+        dp = vector(n + 1, vector<int>(10, 0));
+        for (int i = 0; i < 10; i++) dp[0][i] = 1;
+
+        for (int rem = 1; rem < n; rem++) {
+            for (int square = 0; square < 10; square++) {
+                int ans = 0;
+                for (int nextSquare : jumps[square]) {
+                    ans = (ans + dp[rem - 1][nextSquare]) % MOD;
+                }
+
+                dp[rem][square] = ans;
+            }
+        }
+
         int ans = 0;
         for (int square = 0; square < 10; square++) {
-            ans = (ans + f(n - 1, square)) % MOD;
+            ans = (ans + dp[n - 1][square]) % MOD;
         }
 
         return ans;
