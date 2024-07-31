99d2c4685d805bae89b8ca98e8fc88f639594279
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-31 16:52:10 +0530

    Time: 11 ms (32.13%) | Memory: 18.7 MB (10.55%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -10,8 +10,8 @@ private:
         }
 
         // Calculate the score difference if the current player picks i or j
-        int pickLeft = piles[i] - f(i + 1, j, piles, dp);
-        int pickRight = piles[j] - f(i, j - 1, piles, dp);
+        int pickLeft = piles[i] + f(i + 1, j, piles, dp);
+        int pickRight = piles[j] + f(i, j - 1, piles, dp);
 
         // Return the best option for the current player
         return dp[i][j] = max(pickLeft, pickRight);
@@ -21,6 +21,6 @@ public:
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, n - 1, piles, dp) > 0;
+        return f(0, n - 1, piles, dp) > accumulate(piles.begin(), piles.end(), 0) / 2;
     }
 };
