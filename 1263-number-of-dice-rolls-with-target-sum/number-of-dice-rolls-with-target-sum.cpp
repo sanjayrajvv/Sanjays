f6a22d15f5d047e705f085e94cf36f4a16716262
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-31 19:26:16 +0530

    Time: 17 ms (50.38%) | Memory: 9.8 MB (48.18%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,32 +1,24 @@
 class Solution {
     int MOD = 1e9 + 7;
-private:
-    int f(int i, int target, int n, int k, vector<vector<int>> &dp) {
-        if (i == n) {
-            // If all dice have been used and target is achieved
-            return target == 0;
-        }
-        if (target < 0) {
-            return 0;
-        }
-
-        if (dp[i][target] != -1) {
-            return dp[i][target];
-        }
-
-        int count = 0;
-        // Try each possible face value from 1 to k
-        for (int face = 1; face <= k; face++) {
-            count = (count + f(i + 1, target - face, n, k, dp)) % MOD;
-        }
-
-        return dp[i][target] = count;
-    }
-
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        dp[n][0] = 1;
 
-        return f(0, target, n, k, dp);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int currTarget = 0; currTarget <= target; currTarget++) {
+                int count = 0;
+                // Try each possible face value from 1 to k
+                for (int face = 1; face <= k; face++) {
+                    if (currTarget >= face) {
+                        count = (count + dp[i + 1][currTarget - face]) % MOD;
+                    }
+                }
+
+                dp[i][currTarget] = count;
+            }
+        }
+
+        return dp[0][target];
     }
 };
