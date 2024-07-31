05dbaacd1e4aeef2fb7367f24b1aad7495de291e
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-07-31 17:47:05 +0530

    Time: 51 ms (89.54%) | Memory: 12.7 MB (78.81%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,31 +1,19 @@
 class Solution {
-private:
-    int f(int i, int m, int n, vector<string> &strs, vector<vector<vector<int>>> &dp) {
-        if (i < 0) {
-            return 0;
-        }
-
-        if (dp[i][m][n] != -1) {
-            return dp[i][m][n];
-        }
-
-        int zeros = count(strs[i].begin(), strs[i].end(), '0');
-        int ones = strs[i].size() - zeros;
-
-        int notTake = f(i - 1, m, n, strs, dp);
-        int take = INT_MIN;
-        if (m >= zeros && n >= ones) {
-            take = 1 + f(i - 1, m - zeros, n - ones, strs, dp);
-        }
-
-        return dp[i][m][n] = max(notTake, take);
-    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int size = strs.size();
-
-        vector<vector<vector<int>>> dp(size, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-
-        return f(size - 1, m, n, strs, dp);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        for (const string& str : strs) {
+            int zeros = count(str.begin(), str.end(), '0');
+            int ones = str.size() - zeros;
+            
+            for (int i = m; i >= zeros; --i) {
+                for (int j = n; j >= ones; --j) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones]);
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
