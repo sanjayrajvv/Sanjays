b8d59c44758cdaf12482dc28c492ca2dfd09cb68
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-01 23:12:19 +0530

    Time: 346 ms (56.02%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,9 +1,7 @@
-# Write your MySQL query statement below
 SELECT 
     query_name, 
     ROUND(SUM(rating / position) / COUNT(query_name), 2) AS quality,
-    ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*)), 4) * 100 
-    AS poor_query_percentage
+    ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*)), 4) * 100 AS poor_query_percentage
 FROM 
     Queries
 WHERE 
