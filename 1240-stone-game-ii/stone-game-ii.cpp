c5987aaec382e20b05e13e8e6c9d135da68157bf
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-01 11:10:54 +0530

    Time: 298 ms (5.02%) | Memory: 48.3 MB (9.96%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,35 +1,31 @@
 class Solution {
-private:
-    int f(int i, int m, bool turn, vector<int> &piles, vector<vector<vector<int>>>& dp) {
-        int n = piles.size();
-        if (i == n) {
-            return 0;
-        }
-
-        if (dp[i][m][turn] != -1) {
-            return dp[i][m][turn];
-        }
-
-        int maxStones = turn == true ? 0 : INT_MAX;
-        int stones = 0;
-        for (int j = i; j < min(i + 2 * m, n); j++) {
-            stones += piles[j];
-            if (turn) {
-                maxStones = max(maxStones, stones + 
-                f(j + 1, max(j - i + 1, m), !turn, piles, dp)); 
-            } else {
-                maxStones = min(maxStones, 
-                f(j + 1, max(j - i + 1, m), !turn, piles, dp));
-            }
-        }
-
-        return dp[i][m][turn] = maxStones;
-    }
 public:
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2 * n, vector<int>(2, -1)));
-        return f(0, 1, 1, piles, dp);
+        vector<vector<vector<int>>> dp(n + 1, 
+        vector<vector<int>>(2 * n + 1, vector<int>(2, 0)));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int m = 2 * n; m >= 1; m--) {
+                for (int turn = 0; turn < 2; turn++) {
+                    int maxStones = turn == true ? 0 : INT_MAX;
+                    int stones = 0;
+                    for (int j = i; j < min(i + 2 * m, n); j++) {
+                        stones += piles[j];
+                        if (turn) {
+                            maxStones = max(maxStones, stones + 
+                            dp[j + 1][max(j - i + 1, m)][!turn]); 
+                        } else {
+                            maxStones = min(maxStones, 
+                            dp[j + 1][max(j - i + 1, m)][!turn]);
+                        }
+                    }
+
+                    dp[i][m][turn] = maxStones;
+                }
+            }
+        }
+
+        return dp[0][1][1];
     }
 };
