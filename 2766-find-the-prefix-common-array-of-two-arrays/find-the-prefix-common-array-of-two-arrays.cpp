094649cabe5fe37cd226f6c30551254dd5c11493
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-02 14:02:15 +0530

    Time: 29 ms (74.21%) | Memory: 86.2 MB (63.24%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        set<int> setA, setB;
-
         vector<int> prefix(n);
-
-        for (int i = 0; i < n; i++) {
-            setA.insert(A[i]);
-            setB.insert(B[i]);
-
-            int count = 0;
-            for (int val : setA) {
-                count += setB.count(val);
-            }
-
-            prefix[i] = count;
+        
+        bitset<10001> seenA, seenB;  // Assuming elements in A and B are between 1 and 10000
+        
+        for (int i = 0; i < n; ++i) {
+            seenA[A[i]] = 1;
+            seenB[B[i]] = 1;
+            
+            // Perform bitwise AND and count the number of set bits (common elements)
+            prefix[i] = (seenA & seenB).count();
         }
-
+        
         return prefix;
     }
 };
