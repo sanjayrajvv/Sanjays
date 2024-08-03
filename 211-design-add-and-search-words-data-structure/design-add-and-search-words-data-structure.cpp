d2780219b72163c48f7b22df705bca70aed86fc6
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-03 10:07:48 +0530

    Time: 830 ms (31.01%) | Memory: 609.5 MB (22.33%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -3,32 +3,37 @@ struct Node {
     bool flag = false;
 
     bool containsKey(char ch) {
-        return (links[ch - 'a'] != NULL);
-    }
-
-    Node *get(char ch) {
-        return links[ch - 'a'];
+        return links[ch - 'a'] != nullptr;
     }
 
     void put(char ch, Node *node) {
         links[ch - 'a'] = node;
     }
 
-    void setEnd() {
-        flag = true;
+    Node *get(char ch) {
+        return links[ch - 'a'];
     }
 
     bool isEnd() {
         return flag;
     }
-};
-class WordDictionary {
-    Node *root;
-private:
-    bool searchInNode(string word, int i, Node *node) {
-        if (node == NULL) return false;
 
-        if (i == word.size()) return node->isEnd();
+    void setEnd() {
+        flag = true;
+    }
+};
+
+class WordDictionary {
+private:
+    Node *root;
+
+    bool searchInNode(string word, int i, Node *node) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (i == word.size()) {
+            return node->isEnd();
+        }
 
         if (word[i] != '.') {
             return searchInNode(word, i + 1, node->links[word[i] - 'a']);
@@ -42,6 +47,7 @@ private:
 
         return false;
     }
+
 public:
     WordDictionary() {
         root = new Node();
@@ -49,20 +55,18 @@ public:
     
     void addWord(string word) {
         Node *node = root;
-        
         for (int i = 0; i < word.size(); i++) {
             if (!node->containsKey(word[i])) {
                 node->put(word[i], new Node());
             }
-
             node = node->get(word[i]);
         }
-
         node->setEnd();
     }
     
     bool search(string word) {
-        return searchInNode(word, 0, root);
+        Node *node = root;
+        return searchInNode(word, 0, node);
     }
 };
 
