c7b2a17cddddb9cbd91d36a1f6d344d4c0383fd6
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-07 17:27:44 +0530

    Time: 4 ms (99.52%) | Memory: 39.9 MB (41.14%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,23 +1,48 @@
 class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        int totalCount = 1;
+        map<int, map<int, int>> freqMap;
+
+        // Calculate frequencies based on remainder
+        for (int& num : nums) {
+            freqMap[num % k][num]++;
+        }
+
+        // Calculate subsets for each remainder group
+        for (auto& fr : freqMap) {
+            vector<pair<int, int>> subsets(fr.second.begin(), fr.second.end());
+            totalCount *= countBeautifulSubsets(subsets, subsets.size(), k, 0);
+        }
+
+        return totalCount - 1;
+    }
+
 private:
-    int f(int i, unordered_map<int, int>& count, vector<int>& nums, int k) {
-        if (i == nums.size()) {
+    int countBeautifulSubsets(vector<pair<int, int>>& subsets, int numSubsets,
+                              int difference, int i) {
+        // Base case: Return 1 for a subset of size 1
+        if (i == numSubsets) {
             return 1;
         }
 
-        int beutifulSubsets = f(i + 1, count, nums, k);
-        if (count[nums[i] + k] == 0 && count[nums[i] - k] == 0) {
-            count[nums[i]]++;
-            beutifulSubsets += f(i + 1, count, nums, k);
-            count[nums[i]]--;
+        // Calculate subsets where the current subset is not taken
+        int skip =
+            countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
+        // Calculate subsets where the current subset is taken
+        int take = (1 << subsets[i].second) - 1;
+
+        // If next number has a 'difference', calculate subsets; otherwise, move
+        // to next
+        if (i + 1 < numSubsets &&
+            subsets[i + 1].first - subsets[i].first == difference) {
+            take *=
+                countBeautifulSubsets(subsets, numSubsets, difference, i + 2);
+        } else {
+            take *=
+                countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
         }
 
-        return beutifulSubsets;
-    }
-public:
-    int beautifulSubsets(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        sort(nums.begin(), nums.end());
-        return f(0, count, nums, k) - 1; //removing emopty substring
+        return skip + take;  // Return total count of subsets
     }
 };
