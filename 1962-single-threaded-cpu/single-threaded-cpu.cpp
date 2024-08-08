7d17140ac019d20fb0ee4277a63184b4e91f6ee0
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-08 17:42:44 +0530

    Time: 461 ms (21.61%) | Memory: 156.6 MB (21.54%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         // Define a priority queue with a custom comparator
         priority_queue<vector<int>, vector<vector<int>>, 
-                       greater<vector<int>>> pq;
-
+                        greater<vector<int>>> pq;
+        
         // Add the original indices to the tasks for tracking
         for (int i = 0; i < tasks.size(); ++i) {
             tasks[i].push_back(i);
@@ -12,26 +12,23 @@ public:
 
         // Sort the tasks by their enqueue time
         sort(tasks.begin(), tasks.end());
+        
+        long long currTime = tasks[0][0];
+        pq.push({tasks[0][1], tasks[0][2], tasks[0][1]});
+        int i = 1;
 
-        long long currTime = 0;
-        int i = 0;
         vector<int> schedule;
-
-        while (i < tasks.size() || !pq.empty()) {
-            // Add all tasks that are available by currTime
+        while (!pq.empty() || i < tasks.size()) {
             while (i < tasks.size() && currTime >= tasks[i][0]) {
-                pq.push({tasks[i][1], tasks[i][2]});
+                pq.push({tasks[i][1], tasks[i][2], tasks[i][1]});
                 i++;
             }
 
-            // If the priority queue is empty and there are still tasks left,
-            // jump the current time to the next task's enqueue time
             if (pq.empty()) {
                 currTime = tasks[i][0];
                 continue;
             }
 
-            // Process the task with the smallest processing time (and smallest index in case of a tie)
             int processingTime = pq.top()[0];
             int index = pq.top()[1];
             pq.pop();
