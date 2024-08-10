1e29f8f89faf16285e71344a6164f509760ef863
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-10 22:17:32 +0530

    Time: 2 ms (62.17%) | Memory: 7.3 MB (54.01%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int numTrees(int n) {
-        return nCr(2 * n, n) / (n + 1);
-    }
+        vector<int> numTree(n + 1, 1);
 
-private:
-    long long nCr(int n, int r) {
-        if (r > n - r) 
-            r = n - r; // because nCr = nC(n-r)
-        
-        long long result = 1;
-        
-        for (int i = 0; i < r; ++i) {
-            result *= (n - i);
-            result /= (i + 1);
+        for (int node = 2; node <= n; node++) {
+            int total = 0;
+            for (int root = 1; root <= node; root++) {
+                int left = root - 1;
+                int right = node - root;
+
+                total += numTree[left] * numTree[right];
+            }
+            numTree[node] = total;
         }
-        
-        return result;
+
+        return numTree[n];
     }
 };
