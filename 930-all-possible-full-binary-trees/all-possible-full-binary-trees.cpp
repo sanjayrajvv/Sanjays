e18aa5dca55f39ddde6b029cb296f7f9d22410d6
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-11 17:18:06 +0530

    Time: 55 ms (70.23%) | Memory: 32.2 MB (60.75%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 private:
     unordered_map<int, vector<TreeNode*>> dp;
@@ -27,7 +16,7 @@ public:
         }
 
         vector<TreeNode*> res;
-        for (int i = 1; i < n; i+= 2) {
+        for (int i = 0; i < n; i++) {
             vector<TreeNode*> left = allPossibleFBT(i);
             vector<TreeNode*> right = allPossibleFBT(n - i - 1);
 
