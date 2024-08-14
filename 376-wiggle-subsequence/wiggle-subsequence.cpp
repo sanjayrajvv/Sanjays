ebf669a70739ad69b5ed198b0638d7d159cb373a
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-14 18:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.14%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -2,33 +2,19 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
 
-        return f(0, -1, nums, dp); 
-    }
+        if (n < 2) return n;
 
-private:
-    int f(int index, int prevIndex, vector<int>& nums, vector<vector<int>>& dp) {
-        int n = nums.size();
-        if (index == n) {
-            return 0;
+        int up = 1, down = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
         }
 
-        if (dp[index][prevIndex + 1] != -1) {
-            return dp[index][prevIndex + 1];
-        }
-
-        int notTake = f(index + 1, prevIndex, nums, dp);
-        int take = INT_MIN;
-
-        if (prevIndex == -1 || 
-            (nums[index] > nums[prevIndex] && 
-             (index == n - 1 || nums[index + 1] < nums[index])) || 
-            (nums[index] < nums[prevIndex] && 
-             (index == n - 1 || nums[index + 1] > nums[index]))) {
-            take = 1 + f(index + 1, index, nums, dp);
-        }
-
-        return dp[index][prevIndex + 1] = max(take, notTake);
+        return max(up, down);
     }
 };
