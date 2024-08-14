db8fca2fa528655884b658e73a59039597ecc268
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-14 17:05:07 +0530

    Time: 52 ms (59.95%) | Memory: 41.1 MB (37.34%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -3,9 +3,19 @@ public:
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(3, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(3, INT_MIN));
+        dp[n][0] = 0;
 
-        return f(0, 0, nums, dp);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int remainder = 0; remainder < 3; remainder++) {
+                int take = nums[i] + dp[i + 1][(remainder + nums[i]) % 3];
+                int notTake = dp[i + 1][remainder];
+
+                dp[i][remainder] = max(take, notTake);
+            }
+        }
+
+        return dp[0][0];
     }
 
 private:
