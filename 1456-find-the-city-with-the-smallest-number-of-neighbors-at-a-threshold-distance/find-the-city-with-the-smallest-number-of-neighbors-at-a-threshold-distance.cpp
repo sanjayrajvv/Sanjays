e540920c53b1ab70a7b84a18c0e6c9e2647be3e1
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-16 10:53:17 +0530

    Time: 20 ms (90.14%) | Memory: 16.1 MB (44.00%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> A(n, vector<int>(n, INT_MAX));
-
-        //Creating distance matrix
-        for (int i = 0; i < n; i++) A[i][i] = 0;
+        vector<vector<int>> mat(n, vector<int>(n, INT_MAX));
         for (auto edge : edges) {
-            int i = edge[0];
-            int j = edge[1];
+            int u = edge[0];
+            int v = edge[1];
             int w = edge[2];
 
-            A[i][j] = w;
-            A[j][i] = w;
+            mat[u][v] = w;
+            mat[v][u] = w;
         }
 
-        //Floy-Warshall Algorithm
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (A[i][k] != INT_MAX && A[k][j] != INT_MAX) {
-                        A[i][j] = min(A[i][j], A[i][k] + A[k][j]);
+                    if (mat[i][k] != INT_MAX && mat[k][j] != INT_MAX) {
+                        mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
                     }
                 }
             }
         }
 
-        int city = 0, count = INT_MAX;
-
+        int city = 0;
+        int cityCount = INT_MAX;
         for (int i = 0; i < n; i++) {
-            int current = 0;
+            int count = 0;
             for (int j = 0; j < n; j++) {
-                if (i != j && A[i][j] <= distanceThreshold) {
-                    current++;
+                if ( i != j && mat[i][j] <= distanceThreshold) {
+                    count++;
                 }
             }
-            
-            if (current < count || (count == current && i > city)) {
-                count = current;
+            if (count < cityCount || (count == cityCount && i > city)) {
                 city = i;
+                cityCount = count;
             }
         }
 
