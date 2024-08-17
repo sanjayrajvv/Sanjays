467e2248d11b7427aec42ccf1c6f5a6e26a1bd30
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-17 14:57:51 +0530

    Time: 135 ms (66.13%) | Memory: 99 MB (28.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,28 +2,33 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
+
         int sum = accumulate(nums.begin(), nums.end(), 0);
+
         if (sum % 2) return false;
+
         int target = sum / 2;
 
-        vector<bool> prev(target + 1, false);
-        vector<bool> curr(target + 1, false);
-        prev[0] = curr[0] = true;
-        if (nums[0] <= target) prev[nums[0]] = true;
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
 
-        for (int index = 1; index < n; index++) {
-            for (int currTarget = 0; currTarget <= target; currTarget++) {
-                bool notTake = prev[currTarget];
-                bool take = false; 
-                if (currTarget >= nums[index]) {
-                    take = prev[currTarget - nums[index]];
-                }
+        return f(n - 1, target, nums, dp);
+    }
 
-                curr[currTarget] = notTake || take;
-            }
-            prev = curr;
+private:
+    bool f(int index, int target, vector<int>& nums, vector<vector<int>>& dp) {
+        if (target == 0) return true;
+        if (index == 0) return nums[0] == target;
+
+        if (dp[index][target] != -1) return dp[index][target];
+
+        bool notTake = f(index - 1, target, nums, dp);
+        bool take = false;
+        if (target >= nums[index]) {
+            take = f(index - 1, target - nums[index], nums, dp);
         }
 
-        return prev[target];
+        return dp[index][target] = notTake | take;
     }
 };
+
+
