8b9adddc5f7fb9f1fe35f7d8304dde9ef6250033
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-18 23:40:08 +0530

    Time: 6 ms (55.94%) | Memory: 9.8 MB (56.48%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        // Min-heap to store and retrieve the smallest ugly number
-        priority_queue<long, vector<long>, greater<long>> minHeap;
-        unordered_set<long> seenNumbers;  // Set to avoid duplicates
-        vector<int> primeFactors = {
-            2, 3, 5};  // Factors for generating new ugly numbers
+        vector<int> uglyNumbers(n);  // DP array to store ugly numbers
+        uglyNumbers[0] = 1;          // The first ugly number is 1
 
-        minHeap.push(1);
-        seenNumbers.insert(1);
+        // Three pointers for the multiples of 2, 3, and 5
+        int indexMultipleOf2 = 0, indexMultipleOf3 = 0, indexMultipleOf5 = 0;
+        int nextMultipleOf2 = 2, nextMultipleOf3 = 3, nextMultipleOf5 = 5;
 
-        long currentUgly = 1;
-        for (int i = 0; i < n; ++i) {
-            currentUgly = minHeap.top();  // Get the smallest number
-            minHeap.pop();                // Remove it from the heap
+        // Generate ugly numbers until we reach the nth one
+        for (int i = 1; i < n; i++) {
+            // Find the next ugly number as the minimum of the next multiples
+            int nextUglyNumber =
+                min(nextMultipleOf2, min(nextMultipleOf3, nextMultipleOf5));
+            uglyNumbers[i] = nextUglyNumber;
 
-            // Generate and push the next ugly numbers
-            for (int prime : primeFactors) {
-                long nextUgly = currentUgly * prime;
-                if (seenNumbers.find(nextUgly) ==
-                    seenNumbers.end()) {  // Avoid duplicates
-                    minHeap.push(nextUgly);
-                    seenNumbers.insert(nextUgly);
-                }
+            // Update the corresponding pointer and next multiple
+            if (nextUglyNumber == nextMultipleOf2) {
+                indexMultipleOf2++;
+                nextMultipleOf2 = uglyNumbers[indexMultipleOf2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                indexMultipleOf3++;
+                nextMultipleOf3 = uglyNumbers[indexMultipleOf3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                indexMultipleOf5++;
+                nextMultipleOf5 = uglyNumbers[indexMultipleOf5] * 5;
             }
         }
 
-        return static_cast<int>(currentUgly);  // Return the nth ugly number
+        return uglyNumbers[n - 1];  // Return the nth ugly number
     }
 };
