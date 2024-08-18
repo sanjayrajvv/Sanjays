c1c4b84bfe4072b54df3338164410a72dad7e4ed
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-18 23:39:52 +0530

    Time: 72 ms (31.09%) | Memory: 34.2 MB (12.97%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long> uglyNumbersSet;  // Set to store potential ugly numbers
-        uglyNumbersSet.insert(1);  // Start with 1, the first ugly number
+        // Min-heap to store and retrieve the smallest ugly number
+        priority_queue<long, vector<long>, greater<long>> minHeap;
+        unordered_set<long> seenNumbers;  // Set to avoid duplicates
+        vector<int> primeFactors = {
+            2, 3, 5};  // Factors for generating new ugly numbers
+
+        minHeap.push(1);
+        seenNumbers.insert(1);
 
         long currentUgly = 1;
-        for (int i = 0; i < n; i++) {
-            currentUgly =
-                *uglyNumbersSet
-                     .begin();  // Get the smallest number from the set
-            uglyNumbersSet.erase(
-                uglyNumbersSet.begin());  // Remove it from the set
+        for (int i = 0; i < n; ++i) {
+            currentUgly = minHeap.top();  // Get the smallest number
+            minHeap.pop();                // Remove it from the heap
 
-            // Insert the next potential ugly numbers
-            uglyNumbersSet.insert(currentUgly * 2);
-            uglyNumbersSet.insert(currentUgly * 3);
-            uglyNumbersSet.insert(currentUgly * 5);
+            // Generate and push the next ugly numbers
+            for (int prime : primeFactors) {
+                long nextUgly = currentUgly * prime;
+                if (seenNumbers.find(nextUgly) ==
+                    seenNumbers.end()) {  // Avoid duplicates
+                    minHeap.push(nextUgly);
+                    seenNumbers.insert(nextUgly);
+                }
+            }
         }
 
         return static_cast<int>(currentUgly);  // Return the nth ugly number
