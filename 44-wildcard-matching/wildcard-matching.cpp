7c7b48f6b6e677625b647c4b4c46d3f33ec35cc4
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-19 11:50:01 +0530

    Time: 63 ms (35.96%) | Memory: 16.6 MB (58.01%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int sz = s.size(), pz = p.size();
+        int m = s.size(), n = p.size();
 
-        vector<bool> prev(pz + 1, 0);
-        vector<bool> curr(pz + 1, 0);
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
 
-        prev[0] = true;
-        for (int j = 1; j <= pz; j++) {
-            bool flag = true;
-            for (int jj = 1; jj <= j; jj++) {
-                if (p[jj - 1] != '*') {
-                    flag = false;
-                    break;
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
                 }
             }
-            prev[j] = flag;
         }
 
-        for (int i = 1; i <= sz; i++) {
-            for (int j = 1; j <= pz; j++) {
-                bool match = false;
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                    match = prev[j - 1];
-                } 
-                if (p[j - 1] == '*') {
-                    match = prev[j] | curr[j - 1];
-                } 
-                curr[j] = match;
-            }
-            prev = curr;
-        }
-
-        return prev[pz];
+        return dp[m][n];
     }
 };
