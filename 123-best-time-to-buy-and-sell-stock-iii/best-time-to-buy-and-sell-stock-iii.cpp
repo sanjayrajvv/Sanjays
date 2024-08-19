dd90eb5160c13bd00e3498c23a42818735c62f6c
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-19 13:57:53 +0530

    Time: 431 ms (49.74%) | Memory: 210 MB (42.08%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,9 +3,31 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, 
+                                                vector<int>(3, 0)));
 
-        return f(0, 1, 2, prices, dp);
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int count = 2; count > 0; count--) {
+                    int profit = 0;
+                    if (buy) {
+                        int buyToday = -prices[index] + dp[index + 1][!buy][count];
+                        int notBuy = dp[index + 1][buy][count];
+
+                        profit = max(buyToday, notBuy);
+                    } else {
+                        int sellToday = prices[index] + dp[index + 1][!buy][count - 1];
+                        int notSell = dp[index + 1][buy][count];
+
+                        profit = max(sellToday, notSell);
+                    }
+
+                    dp[index][buy][count] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 
 private:
