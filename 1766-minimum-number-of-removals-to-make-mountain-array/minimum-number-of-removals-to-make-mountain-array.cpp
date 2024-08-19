5aad9b880311310bbfadc4f96fce7c506afd851b
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-19 17:34:07 +0530

    Time: 68 ms (60.40%) | Memory: 14.7 MB (64.95%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -2,32 +2,34 @@ class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size();
-        
+
         vector<int> dp1(n, 1);
+        vector<int> dp2(n, 1);
+
         for (int index = 1; index < n; index++) {
             for (int prevIndex = 0; prevIndex < index; prevIndex++) {
-                if (nums[prevIndex] < nums[index]) {
+                if (nums[index] > nums[prevIndex]) {
                     dp1[index] = max(dp1[index], 1 + dp1[prevIndex]);
                 }
             }
         }
 
-        vector<int> dp2(n, 1);
         for (int index = n - 2; index >= 0; index--) {
             for (int prevIndex = n - 1; prevIndex > index; prevIndex--) {
-                if (nums[prevIndex] < nums[index]) {
+                if (nums[index] > nums[prevIndex]) {
                     dp2[index] = max(dp2[index], 1 + dp2[prevIndex]);
                 }
             }
         }
 
-        int longestBitonic = 0;
+        int maxLen = 1;
         for (int i = 0; i < n; i++) {
-            if (dp1[i] > 1 && dp2[i] > 1) { // Ensure both sides of the peak are valid
-                longestBitonic = max(longestBitonic, dp1[i] + dp2[i] - 1);
+            if (dp1[i] > 1 && dp2[i] > 1) {
+                maxLen = max(maxLen, dp1[i] + dp2[i] - 1);
             }
         }
 
-        return n - longestBitonic;
+
+        return n - maxLen;
     }
 };
