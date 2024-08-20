da1d683304e56279f84c8470822f43240399db61
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-20 23:49:24 +0530

    Time: 3 ms (44.70%) | Memory: 8.1 MB (64.20%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,23 +3,22 @@ public:
     int numDecodings(string s) {
         int n = s.size();
 
-        vector<int> dp(n, -1);
+        vector<int> dp(n + 1, 1);
 
-        return f(0, s, dp);
-    }
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+                continue;
+            }
+                
+            int ways = dp[i + 1];
+            if (i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')) {
+                ways += dp[i + 2];
+            }
 
-private:
-    int f(int i, string s, vector<int>& dp) {
-        if (i == s.size()) return 1;
-        if (s[i] == '0') return 0;
-
-        if (dp[i] != -1) return dp[i];
-
-        int ways = f(i + 1, s, dp);
-        if (i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')) {
-            ways += f(i + 2, s, dp);
+            dp[i] = ways;
         }
 
-        return dp[i] = ways;
+        return dp[0];
     }
 };
