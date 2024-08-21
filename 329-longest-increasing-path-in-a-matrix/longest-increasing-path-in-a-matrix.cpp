4a4d374eda336dbfddd2ba820c7fbc8beaecb210
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-21 14:40:36 +0530

    Time: 28 ms (73.62%) | Memory: 19.3 MB (74.24%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,25 +1,33 @@
 class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        int maxPath = 1;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                maxPath = max(maxPath, dfs(r, c, matrix, dp));
+            }
+        }
+
+        return maxPath;
+    }
+
 private:
     int delRow[4] = {-1, 0, 1, 0};
     int delCol[4] = {0, 1, 0, -1};
 
-    bool isBound(int i, int j, int m, int n) {
-        return i >= 0 && i < m && j >= 0 && j < n;
-    }
-
     int dfs(int r, int c, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
         if (dp[r][c] != -1) return dp[r][c];
-
-        int m = matrix.size();
-        int n = matrix[0].size();
 
         int maxPath = 1;
         for (int i = 0; i < 4; i++) {
             int nr = r + delRow[i];
             int nc = c + delCol[i];
 
-            if (isBound(nr, nc, m, n) && 
-            matrix[nr][nc] > matrix[r][c]) {
+            if (bound(nr, nc, matrix) && matrix[r][c] < matrix[nr][nc]) {
                 maxPath = max(maxPath, 1 + dfs(nr, nc, matrix, dp));
             }
         }
@@ -27,21 +35,9 @@ private:
         return dp[r][c] = maxPath;
     }
 
-public:
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return 0;
-        
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int maxPath = 1;
+    bool bound(int r, int c, vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                maxPath = max(maxPath, dfs(i, j, matrix, dp));
-            }
-        }
-
-        return maxPath;
+        return r >= 0 && r < m && c >= 0 && c < n;
     }
 };
