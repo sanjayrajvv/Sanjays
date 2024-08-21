3f347d4800f9eae8cda6fa778de3975336a802ea
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-21 13:41:14 +0530

    Time: 6 ms (49.67%) | Memory: 8.4 MB (74.40%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,3 +1,36 @@
+/*class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.size(), n = s2.size();
+
+        if (m + n != s3.size()) return false;
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[m][n] = true;
+
+        for (int i = m; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                
+                bool l = false;
+                if (i < m && s1[i] == s3[i + j]) {
+                    l = dp[i + 1][j];
+                }
+
+                bool r = false;
+                if (j < n && s2[j] == s3[i + j]) {
+                    r = dp[i][j + 1];
+                }
+
+                dp[i][j] = l | r;
+            }
+        }
+
+
+        return dp[0][0];
+    }
+};*/
+
+
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -5,29 +38,22 @@ public:
 
         if (m + n != s3.size()) return false;
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[m][n] = true;
 
-        return f(0, 0, s1, s2, s3, dp);
-    }
+        for (int i = m; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                
+                if (i < m && s1[i] == s3[i + j]) {
+                    dp[i][j] = dp[i + 1][j];
+                }
 
-private:
-    bool f(int i, int j, string s1, string s2, string s3, 
-            vector<vector<int>>& dp) {
-        if (i + j == s3.size()) return true;
-        if (s1[i] != s3[i + j] && s2[j] != s3[i + j]) return false;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        bool l = false;
-        if (s1[i] == s3[i + j]) {
-            l = f(i + 1, j, s1, s2, s3, dp);
+                if (j < n && s2[j] == s3[i + j]) {
+                    dp[i][j] = dp[i][j] || dp[i][j + 1];
+                }
+            }
         }
 
-        bool r = false;
-        if (s2[j] == s3[i + j]) {
-            r = f(i, j + 1, s1, s2, s3, dp);
-        }
-
-        return dp[i][j] = l | r;
+        return dp[0][0];
     }
 };
