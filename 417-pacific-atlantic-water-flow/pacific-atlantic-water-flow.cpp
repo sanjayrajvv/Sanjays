19c346be5054138047e0cb4f2a4de620ce502a2d
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-22 17:15:07 +0530

    Time: 25 ms (85.43%) | Memory: 21.9 MB (66.96%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,51 +1,55 @@
 class Solution {
-private:
-    bool isValid(int r, int c, int m, int n) {
-        return r >= 0 && r < m && c >= 0 && c < n;
-    }
-
-    void dfs(int r, int c, vector<vector<int>> &vis, int prevHeight, 
-    vector<vector<int>>& heights) {
-        int m = heights.size(), n = heights[0].size();
-        vis[r][c] = 1;
-
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-
-        for (int i = 0; i < 4; i++) {
-            int nr = r + delRow[i];
-            int nc = c + delCol[i];
-
-            if (isValid(nr, nc, m, n) && !vis[nr][nc] && prevHeight <= heights[nr][nc]) {
-                dfs(nr, nc, vis, heights[nr][nc], heights);
-            }
-        }
-    }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size(), n = heights[0].size();
+
         vector<vector<int>> visPacific(m, vector<int>(n, 0));
         vector<vector<int>> visAtlantic(m, vector<int>(n, 0));
 
         for (int r = 0; r < m; r++) {
-            dfs(r, 0, visPacific, heights[r][0], heights);
-            dfs(r, n - 1, visAtlantic, heights[r][n - 1], heights);
+            dfs(r, 0, heights, visPacific);
+            dfs(r, n - 1, heights, visAtlantic);
         }
 
         for (int c = 0; c < n; c++) {
-            dfs(0, c, visPacific, heights[0][c], heights);
-            dfs(m - 1, c, visAtlantic, heights[m - 1][c], heights);
+            dfs(0, c, heights, visPacific);
+            dfs(m - 1, c, heights, visAtlantic);
         }
 
-        vector<vector<int>> coordinates;
+        vector<vector<int>> result;
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (visPacific[r][c] && visAtlantic[r][c]) {
-                    coordinates.push_back({r, c});
+                    result.push_back({r, c});
                 }
             }
         }
 
-        return coordinates; 
+        return result;
+    }
+
+private:
+    int delRow[4] = {-1, 0, 1, 0};
+    int delCol[4] = {0, 1, 0, -1};
+
+    void dfs(int row, int col, vector<vector<int>>& heights, 
+                                  vector<vector<int>>& vis) {
+        vis[row][col] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            if (isBound(nrow, ncol, heights) && vis[nrow][ncol] == 0 && 
+                heights[row][col] <= heights[nrow][ncol]) {
+                dfs(nrow, ncol, heights, vis);
+            }
+        }
+    }
+
+    bool isBound(int row, int col, vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+
+        return row >= 0 && row < m && col >= 0 && col < n;
     }
 };
