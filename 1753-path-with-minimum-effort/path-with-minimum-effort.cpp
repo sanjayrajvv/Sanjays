4e49486279f84fe39e32f99261b84431c4f410a7
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-23 17:28:46 +0530

    Time: 74 ms (96.68%) | Memory: 23.9 MB (65.57%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,43 +1,47 @@
 class Solution {
-private:
-    bool isValid(int r, int c, int rows, int cols) {
-        return r >= 0 && r < rows && c >= 0 && c < cols;
-    }
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int rows = heights.size(), columns = heights[0].size();
-        vector<vector<int>> dis(rows, vector<int>(columns, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        int rows = heights.size(), cols = heights[0].size();
+        vector<vector<int>> dis(rows, vector<int>(cols, INT_MAX));
+        priority_queue<pair<int, pair<int, int>>, 
+        vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
 
-        pq.push({0, 0, 0});
+        pq.push({0, {0, 0}});
         dis[0][0] = 0;
+
         while (!pq.empty()) {
-            int effort = pq.top()[0];
-            int row = pq.top()[1];
-            int col = pq.top()[2];
+            int effort = pq.top().first;
+            auto [row, col] = pq.top().second;
             pq.pop();
 
-            if (row == rows - 1 && col == columns - 1) {
+            if (row == rows - 1 && col == cols - 1) {
                 return effort;
             }
-
-            int delRow[] = {-1, 0, 1, 0};
-            int delCol[] = {0, 1, 0, -1};
 
             for (int i = 0; i < 4; i++) {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
 
-                if (isValid(nrow, ncol, rows, columns)) {
-                    int newEffort = max(abs(heights[nrow][ncol] - heights[row][col]), effort);
-                    if (newEffort < dis[nrow][ncol]) {
-                        dis[nrow][ncol] = newEffort;
-                        pq.push({dis[nrow][ncol], nrow, ncol});
+                if (isBound(nrow, ncol, rows, cols)) {
+                    int newEffort = abs(heights[row][col] - heights[nrow][ncol]);
+                    int maxEffort = max(newEffort, effort);
+                    
+                    if (maxEffort < dis[nrow][ncol]) {
+                        dis[nrow][ncol] = maxEffort;
+                        pq.push({maxEffort, {nrow, ncol}});
                     }
                 }
             }
-        }
+        } 
 
         return -1;
+    }
+
+private:
+    int delRow[4] = {-1, 0, 1, 0};
+    int delCol[4] = {0, 1, 0, -1};
+
+    bool isBound(int row, int col, int rows, int cols) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
     }
 };
