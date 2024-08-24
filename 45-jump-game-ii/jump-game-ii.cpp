733e080fcdf7d167079003b746b96c6d62a0c8a6
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-24 19:27:10 +0530

    Time: 11 ms (63.55%) | Memory: 19.3 MB (65.36%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if (nums.size() < 2) return 0;
-
-        int currentJumpEnd = 0, farthest = 0;
+        int l = 0, r = 0;
         int jumps = 0;
+        
+        while (r < nums.size() - 1) {
+            int farthest = 0;
 
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            farthest = max(farthest, i + nums[i]);
-
-            // If we have reached the end of the current jump,
-            // increase the jump count and update the end for the next jump
-            if (i == currentJumpEnd) {
-                ++jumps;
-                currentJumpEnd = farthest;
-
-                // If the farthest point we can reach is the end of the array, break early
-                if (currentJumpEnd >= nums.size() - 1) break;
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, i + nums[i]);
             }
+            l = r + 1;
+            r = farthest;
+            jumps++;
         }
 
         return jumps;
