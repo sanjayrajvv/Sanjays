0cfa911f6030a45117b6159024a48fe7cc8df145
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-25 11:48:51 +0530

    Time: 102 ms (35.38%) | Memory: 26.9 MB (42.58%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
+
+        // Sort pairs based on the second element
+        sort(pairs.begin(), pairs.end(),
+            [](const vector<int> &a, const vector<int> &b) {
+                return a[1] < b[1];
+            }
+        );
+
+        vector<int> dp(n, 1);  // dp[i] will store the length of the longest chain ending with the i-th pair
         int longestChain = 1;
 
-        sort(pairs.begin(), pairs.end(), 
-        [](const vector<int> &a, const vector<int> &b) {
-                return a[1] < b[1];
-        });
-
-        int i = 0, j = 1;
-        while (j < n) {
-            if (pairs[i][1] < pairs[j][0]) {
-                longestChain++;
-                i = j;
-                j++;
-            } else {
-                j++;
+        for (int index = 1; index < n; index++) {
+            for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+                // Check if pairs[prevIndex] can chain to pairs[index]
+                if (pairs[prevIndex][1] < pairs[index][0]) {
+                    dp[index] = max(dp[index], 1 + dp[prevIndex]);
+                }
             }
+            longestChain = max(longestChain, dp[index]);
         }
 
         return longestChain;
