895a2b21fc286680ba8534370dff184ab46a3b92
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-25 13:29:02 +0530

    Time: 125 ms (15.94%) | Memory: 110 MB (72.18%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int n = nums.size();
+        long long len = 0;
+
+        long long ans = 0;
+
+        int i = 0;
+        while (i < n) {
+            if (nums[i] == 0) {
+                len++;
+            } else {
+                ans += (len * (len + 1)) / 2;
+                len = 0;
+            }
+            i++;
+        }
+        ans += (len * (len + 1)) / 2;
+
+        return ans;
+    }
+};
