ff8b315baa744087c6ffa40b9e29e94b8b25c0a9
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-25 11:08:52 +0530

    Time: 342 ms (72.89%) | Memory: 150.3 MB (45.35%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -4,8 +4,8 @@ public:
         unordered_set<int> s;
 
         for (int i = 0; i < triplets.size(); i++) {
-            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] 
-            || triplets[i][2] > target[2]) {
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || 
+            triplets[i][2] > target[2]) {
                 continue;
             }
 
