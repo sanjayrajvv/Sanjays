ef84459ab74a1346fd6b0ae0b84d89361549245f
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-28 17:13:23 +0530

    Time: 123 ms (57.56%) | Memory: 75.1 MB (37.86%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,38 +2,24 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        if (n == 0) return 0;
+        unordered_map<int, int> mp;
 
-        int lastFruit = -1;       // Last fruit type in the sequence
-        int secondLastFruit = -1;  // Second last fruit type
-        int lastFruitCount = 0;    // Count of the last fruit type in the current window
-        int currentMax = 0;        // Max number of fruits in the current window
-        int maxFruits = 0;         // Overall maximum number of fruits
+        int l = 0, maxFruits = 0;
+        for (int r = 0; r < n; r++) {
+            mp[fruits[r]]++;
 
-        for (int i = 0; i < n; i++) {
-            if (fruits[i] == lastFruit || fruits[i] == secondLastFruit) {
-                // If current fruit is one of the two types we're collecting
-                currentMax++;
-            } else {
-                // New fruit type encountered
-                currentMax = lastFruitCount + 1;  // Reset to count of last fruit + current fruit
+            while (mp.size() > 2) {
+                mp[fruits[l]]--;
+                if (mp[fruits[l]] == 0) {
+                    mp.erase(fruits[l]);
+                }
+
+                l++;
             }
 
-            // Update last fruit count
-            if (fruits[i] == lastFruit) {
-                lastFruitCount++;
-            } else {
-                lastFruitCount = 1;  // New last fruit
+            if (mp.size() <= 2) {
+                maxFruits = max(maxFruits, r - l + 1);
             }
-
-            // Update last and second last fruit types
-            if (fruits[i] != lastFruit) {
-                secondLastFruit = lastFruit;
-                lastFruit = fruits[i];
-            }
-
-            // Update the global maximum
-            maxFruits = max(maxFruits, currentMax);
         }
 
         return maxFruits;
