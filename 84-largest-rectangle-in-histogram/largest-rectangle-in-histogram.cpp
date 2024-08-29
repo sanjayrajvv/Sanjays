cc06cf3785ba0155db3827f19ce96e86ab9cf2b5
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-29 07:16:39 +0530

    Time: 115 ms (51.72%) | Memory: 82.4 MB (61.18%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        //{index, height}
         int n = heights.size();
-        stack<pair<int, int>> st;
 
+        //{index, height}
+        stack<pair<int, int>> histograms;
         int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int start = i;
-            while (!st.empty() && st.top().second > heights[i]) {
-                auto top = st.top();
-                st.pop();
 
-                int width = i - top.first;
-                int height = top.second;
+        for (int i = 0; i < heights.size(); i++) {
+            int start = i;
+            while (!histograms.empty() && heights[i] < histograms.top().second) {
+                auto topHist = histograms.top();
+                histograms.pop();
+
+                int width = i - topHist.first;
+                int height = topHist.second;
 
                 maxArea = max(maxArea, height * width);
-                start = top.first;
+                start = topHist.first;
             }
-
-            st.push({start, heights[i]});
+            histograms.push({start, heights[i]});
         }
 
-        while (!st.empty()) {
-            auto top = st.top();
-            st.pop();
+        while (!histograms.empty()) {
+            auto topHist = histograms.top();
+            histograms.pop();
 
-            int width = n - top.first;
-            int height = top.second;
+            int width = n - topHist.first;
+            int height = topHist.second;
 
             maxArea = max(maxArea, height * width);
         }
