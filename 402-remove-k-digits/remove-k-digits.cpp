204ebd0d22873ca019459f74daca9ce42a99297f
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-30 07:58:31 +0530

    Time: 9 ms (75.46%) | Memory: 10.5 MB (28.48%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -6,10 +6,7 @@ public:
 
         stack<char> st;
 
-        // Process each digit in the input number
         for (int i = 0; i < n; i++) {
-            // Remove digits from the stack if they are larger than the current digit
-            // and we still have some digits to remove (k > 0)
             while (!st.empty() && st.top() > num[i] && k > 0) {
                 st.pop();
                 k--;
@@ -17,30 +14,26 @@ public:
             st.push(num[i]);
         }
 
-        // If there are still digits to remove, pop from the end of the stack
-        while (k > 0 && !st.empty()) {  // Added check to prevent stack underflow
+        while (k > 0 && !st.empty()) {
             st.pop();
             k--;
         }
 
-        // Build the result string from the stack
         string ans = "";
         while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
 
-        // Since we built the answer from the stack, it's in reverse order, so reverse it
         reverse(ans.begin(), ans.end());
 
-        // Remove leading zeros from the result
         int start = 0;
         while (start < ans.size() && ans[start] == '0') {
             start++;
         }
+
         ans = ans.substr(start);
 
-        // If the result is empty, return "0"
         return ans.empty() ? "0" : ans;
     }
 };
