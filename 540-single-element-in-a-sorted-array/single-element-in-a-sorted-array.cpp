620092ed59d0cd8727f4011e5f727c58588ee503
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-30 19:33:01 +0530

    Time: 17 ms (72.46%) | Memory: 24.9 MB (69.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -7,18 +7,22 @@ public:
         if (nums[0] != nums[1]) return nums[0];
         if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
 
-        int l = 1, h = n - 2;
-
+        int l = 0, h = nums.size() - 1;
         while (l <= h) {
             int mid = l + (h - l) / 2;
 
-            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid];
-            } else if (nums[mid - 1] == nums[mid] && mid % 2 == 0 ||
-            nums[mid + 1] == nums[mid] && mid % 2 == 1) {
-                h = mid - 1;
             } else {
-                l = mid + 1;
+                if (nums[mid] == nums[mid - 1] && mid % 2 == 0) {
+                    h = mid - 1;
+                } else if (nums[mid] == nums[mid - 1] && mid % 2 == 1) {
+                    l = mid + 1;
+                } else if (nums[mid] == nums[mid + 1] && mid % 2 == 1) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
             }
         }
 
