f092336057cd9dd74ef53e85b6b3d0e335540790
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-30 20:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (37.21%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,23 +1,27 @@
 class Solution {
-    //compute mid
-    //check is mid is the answer if: yes then return 
-    //else: check are in the sorted half,
-    //    if so then check that the element is in that range or not : do repeatedly
-    //     else check the same in the other half
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
+        int l = 0, h = nums.size() - 1;
 
-        int l = 0, h = n - 1;
         while (l <= h) {
             int mid = l + (h - l) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[l] <= nums[mid]) {
-                if (target >= nums[l] && target < nums[mid]) h = mid - 1;
-                else l = mid + 1;
+
+            if (nums[mid] == target) {
+                return mid;
             } else {
-                if (target > nums[mid] && target <= nums[h]) l = mid + 1;
-                else h = mid - 1;
+                if (nums[l] <= nums[mid]) {
+                    if (target >= nums[l] && target < nums[mid]) {
+                        h = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (target <= nums[h] && target > nums[mid]) {
+                        l = mid + 1;
+                    } else {
+                        h = mid - 1;
+                    }
+                }
             }
         }
 
