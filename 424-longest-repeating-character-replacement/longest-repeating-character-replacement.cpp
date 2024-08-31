67d5372db131a6be7550a3967d517ffa22fc8ec1
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-08-31 15:30:33 +0530

    Time: 4 ms (82.91%) | Memory: 8.5 MB (77.61%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        int n = s.size();
+
+        int maxFrequency = 0, maxLen = 0;
+        vector<int> h(26, 0);
+
         int l = 0;
-        int maxFreq = 0, maxLen = 0;
+        for (int r = 0; r < n; r++) {
+            h[s[r] - 'A']++;
+            maxFrequency = max(maxFrequency, h[s[r] - 'A']);
 
-        vector<int> freq(26, 0);
-
-        for (int r = 0; r < s.size(); r++) {
-            freq[s[r] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[r] - 'A']);
-
-            if ((r - l + 1) - maxFreq > k) {
-                freq[s[l] - 'A']--;
+            if ((r - l + 1) - maxFrequency > k) {
+                h[s[l] - 'A']--;
                 l++;
+
+                maxFrequency = 0;
+                for (int i = 0; i < 26; i++) {
+                    maxFrequency = max(maxFrequency, h[i]);
+                }
             }
 
-            maxLen = max(maxLen, r - l + 1);
+            if ((r - l + 1) - maxFrequency <= k) {
+                maxLen = max(maxLen, r - l + 1);
+            }
         }
 
         return maxLen;
