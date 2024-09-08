4256bf1c9166fd1e147b2ac0f4d3480c53c65426
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-08 11:56:52 +0530

    Time: 10 ms (76.19%) | Memory: 26.7 MB (82.70%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,6 +10,31 @@
  * };
  */
 class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        return build(0, n - 1, preorder, 0, n - 1, inorder);
+    }
+
+private:
+    TreeNode* build(int preStart, int preEnd, vector<int>& preorder, 
+                    int inStart, int inEnd, vector<int>& inorder) {
+        if (preStart > preEnd || inStart > inEnd) {
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inRoot = findIndex(preorder[preStart], inorder);
+        int numsLeft = inRoot - inStart;
+
+        root->left = build(preStart + 1, preStart + numsLeft, preorder, 
+                          inStart, inRoot - 1, inorder);
+        root->right = build(preStart + numsLeft + 1, preEnd, preorder, 
+                            inRoot + 1, inEnd, inorder);
+        
+        return root;
+    }
+
     int findIndex(int e, vector<int> &inorder) {
         for (int i = 0; i < inorder.size(); i++) {
             if (inorder[i] == e) {
@@ -18,23 +43,5 @@ class Solution {
         }
 
         return -1;
-    }
-
-    TreeNode *build(int preStart, int preEnd, vector<int>& preorder, int inStart, int inEnd, vector<int>& inorder) {
-        if (preStart > preEnd || inStart > inEnd) {
-            return NULL;
-        }
-
-        TreeNode *root = new TreeNode(preorder[preStart]);
-        int inRoot = findIndex(root->val, inorder);
-        int numsLeft = inRoot - inStart;
-        root->left = build(preStart + 1, preStart + numsLeft, preorder, inStart, inEnd - 1, inorder);
-        root->right = build(preStart + numsLeft + 1, preEnd, preorder, inRoot + 1, inEnd, inorder);
-
-        return root;
-    }
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return build(0, preorder.size() - 1, preorder, 0, inorder.size() - 1, inorder);
     }
 };
