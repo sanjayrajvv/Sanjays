3bbde8c24e79b649a294e4a48ce1e3ee0d5177ae
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-10 16:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (37.53%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     void root_to_leaf(TreeNode *root, vector<string> &ans, string path) {
-        
         if (root == nullptr) {
             return;
         }
 
-        path += to_string(root->val) + "->";
+        path += to_string(root->val);
 
         if (root->left == nullptr && root->right == nullptr) {
-            //path += to_string(root->val);
-            path.pop_back();
-            path.pop_back();
-            
             ans.push_back(path);
-
             return;
-        }
+        } else {
+            path += "->";
 
-        //path.push_back(root->val);
-        root_to_leaf(root->left, ans, path);
-        root_to_leaf(root->right, ans, path);
+            root_to_leaf(root->left, ans, path);
+            root_to_leaf(root->right, ans, path);
+        }
     }
 
     vector<string> binaryTreePaths(TreeNode* root) {
