c9e07b81d88322fb680b253b5c892dad4744aa8b
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-10 18:23:59 +0530

    Time: 8 ms (49.60%) | Memory: 18.5 MB (66.68%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
@@ -19,18 +8,18 @@ public:
 
         while (!q.empty()) {
             int size = q.size();
-            int mmin = q.front().second;
+            int minIndex = q.front().second;
             int first, last;
 
             for (int i = 1; i <= size; i++) {
-                long long curr_id = q.front().second - mmin;
+                long long curr_id = q.front().second - minIndex;
                 TreeNode *node = q.front().first;
                 q.pop();
 
                 if (i == 1) {
                     first = curr_id;
-                }
-
+                } 
+                
                 if (i == size) {
                     last = curr_id;
                 }
