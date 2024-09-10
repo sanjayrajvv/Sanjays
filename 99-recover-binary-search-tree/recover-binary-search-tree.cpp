c562f4e1eedcbaec28381a5dc56341bac2556e5c
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-10 14:35:01 +0530

    Time: 26 ms (34.69%) | Memory: 60.8 MB (30.35%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,27 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 private:
     int index;
 
 public:
+    // O(2n + nlogn)
     void recoverTree(TreeNode* root) {
         vector<int> inorderList;
-        inorder(root, inorderList);
+        inorder(root, inorderList); //O(n)
 
-        sort(inorderList.begin(), inorderList.end());
+        sort(inorderList.begin(), inorderList.end()); //O(nlogn)
 
         index = 0;
-        recover(root, inorderList);
+        recover(root, inorderList); // O(n)
     }
 
 private:
