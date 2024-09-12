4e9330a52dc7c31376389ce277de05a059401f62
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-12 11:02:02 +0530

    Time: 4 ms (56.86%) | Memory: 12.8 MB (79.76%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,20 +1,4 @@
 class Solution {
-    void f(int index, int target, vector<int> &candidates, 
-    vector<int> &ds, vector<vector<int>> &ans) {
-        if (target == 0) {
-            ans.push_back(ds);
-            return;
-        }
-
-        for (int i = index; i < candidates.size(); i++) {
-            if (i != index && candidates[i] == candidates[i - 1]) continue;
-            if (candidates[i] > target) break;
-
-            ds.push_back(candidates[i]);
-            f(i + 1, target - candidates[i], candidates, ds, ans);
-            ds.pop_back();
-        }
-    }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
@@ -22,8 +6,31 @@ public:
 
         sort(candidates.begin(), candidates.end());
 
-        f(0, target, candidates, ds, ans);
+        backtrack(0, target, candidates, ds, ans);
 
         return ans;
+    }
+
+private:
+    void backtrack(int index, int target, vector<int>& candidates, vector<int>& ds, 
+                    vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        if (index == candidates.size() || target < 0) {
+            return;
+        }
+
+        for (int i = index; i < candidates.size(); i++) {
+            if (i != index && candidates[i - 1] == candidates[i]) continue;
+            if (candidates[i] > target) return;
+
+            ds.push_back(candidates[i]);
+            backtrack(i + 1, target - candidates[i], candidates, ds, ans);
+
+            ds.pop_back();
+        }
     }
 };
