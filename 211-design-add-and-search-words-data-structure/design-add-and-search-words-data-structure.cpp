bdd5507f2fafc6cb4b724f299e9ae198442613eb
sanjayrajvv <71118222+sanjayrajvv@users.noreply.github.com>
2024-09-21 15:04:12 +0530

    Time: 440 ms (95.78%) | Memory: 555.2 MB (65.60%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -1,36 +1,54 @@
 struct Node {
-    Node *links[26];
+    Node* links[26];
     bool flag = false;
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
 
     bool containsKey(char ch) {
         return links[ch - 'a'] != nullptr;
     }
 
-    void put(char ch, Node *node) {
-        links[ch - 'a'] = node;
-    }
-
-    Node *get(char ch) {
+    Node* get(char ch) {
         return links[ch - 'a'];
-    }
-
-    bool isEnd() {
-        return flag;
     }
 
     void setEnd() {
         flag = true;
     }
+
+    bool isEnd() {
+        return flag;
+    }
 };
 
 class WordDictionary {
 private:
-    Node *root;
+    Node* root;
 
-    bool searchInNode(string word, int i, Node *node) {
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+
+        node->setEnd();
+    }
+
+    bool searchInNode(string& word, int i, Node* node) {
         if (node == nullptr) {
             return false;
         }
+
         if (i == word.size()) {
             return node->isEnd();
         }
@@ -47,32 +65,9 @@ private:
 
         return false;
     }
-
-public:
-    WordDictionary() {
-        root = new Node();
-    }
-    
-    void addWord(string word) {
-        Node *node = root;
-        for (int i = 0; i < word.size(); i++) {
-            if (!node->containsKey(word[i])) {
-                node->put(word[i], new Node());
-            }
-            node = node->get(word[i]);
-        }
-        node->setEnd();
-    }
     
     bool search(string word) {
-        Node *node = root;
+        Node* node = root;
         return searchInNode(word, 0, node);
     }
 };
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary* obj = new WordDictionary();
- * obj->addWord(word);
- * bool param_2 = obj->search(word);
- */
